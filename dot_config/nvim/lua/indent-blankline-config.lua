4a4b10b2c78d6df3681adbe8534cfcbb0fc2362a
tur1ng <mohammadali.ap.2000@gmail.com>
2022-03-02 14:14:12 +0330

    nvim: add indent space indicators

--- dot_config/nvim/lua/indent-blankline-config.lua
+++ dot_config/nvim/lua/indent-blankline-config.lua
@@ -1,4 +1,8 @@
+vim.opt.list = true
+vim.opt.listchars:append("space:⋅")
+
 require("indent_blankline").setup({
   show_current_context = true,
   show_current_context_start = true,
+  space_char_blankline = " ",
 })
