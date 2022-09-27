98d682942c45aae4fbb7f713931e27f5bb50e21c
tur1ng <mohammadali.ap.2000@gmail.com>
2022-09-27 20:22:36 +0330

    nvim: update nvim-tree

--- dot_config/nvim/lua/nvim-tree-config.lua
+++ dot_config/nvim/lua/nvim-tree-config.lua
@@ -1,4 +1,4 @@
-vim.api.nvim_set_keymap("n", "<C-n>", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>n", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>r", ":NvimTreeRefresh<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>c", ":NvimTreeFindFile<CR>", { noremap = true, silent = true })
 
